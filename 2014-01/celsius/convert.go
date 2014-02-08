7295ad77bf2e558befe530d52240ef00c3e3752e
Nathan Youngman <git@nathany.com>
2014-02-07 21:17:49 -0700

    convert.go
    it's kinda between step one and two

---
+++ 2014-01/celsius/convert.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := 0.0
+	f := CelsiusToFahrenheit(c)
+	fmt.Printf("%vºC is %vºF\n", c, f)
+}
+
+// CelsiusToFahrenheit converts temperature for our American friends.
+func CelsiusToFahrenheit(c float64) float64 {
+	return (c * 9 / 5) + 32
+}
