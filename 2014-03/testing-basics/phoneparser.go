3347a783fed95eaf76aa12f8b748e1f8ea23a887
Matthias Stone <matthias@clockworkendeavors.com>
2014-03-24 16:26:34 -0600

    Make second test pass

--- 2014-03/testing-basics/phoneparser.go
+++ 2014-03/testing-basics/phoneparser.go
@@ -1,11 +1,13 @@
 // Contrived phoneparser package. Do not use
 package phoneparser
 
-import "errors"
+import "strconv"
 
 type PhoneNumber uint64
 
 // Parses string for a valid phone number
 func ParseString(s string) (PhoneNumber, error) {
-	return 0, errors.New("not implemented")
+	number, err := strconv.ParseUint(s, 10, 64)
+
+	return PhoneNumber(number), err
 }
