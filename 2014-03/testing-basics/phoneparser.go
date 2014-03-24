b387dba24811b392bee4f5521cb3210296f98b67
Matthias Stone <matthias@clockworkendeavors.com>
2014-03-24 15:56:07 -0600

    Initial code
    This demonstrates a basic go project with a test file.  Run 'go test' to see
    the test file executed.

---
+++ 2014-03/testing-basics/phoneparser.go
@@ -0,0 +1,9 @@
+// Contrived phoneparser package. Do not use
+package phoneparser
+
+type PhoneNumber uint64
+
+// Parses string for a valid phone number
+func ParseString(s string) (PhoneNumber, error) {
+	return 0, nil
+}
