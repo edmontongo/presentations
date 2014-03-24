8d1c1cf7c2e0c1543bcd997557d0e2aa5a124e18
Matthias Stone <matthias@clockworkendeavors.com>
2014-03-24 16:30:22 -0600

    Change similar tests to be table driven
    Here we use a slice of an anonymous struct with a for/range statement to
    easily represent our tests that return errors as a table. This reduces
    redundant code and improves clarity.

--- 2014-03/testing-basics/parser_test.go
+++ 2014-03/testing-basics/parser_test.go
@@ -6,18 +6,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEmptyStringReturnsError(t *testing.T) {
-	_, err := ParseString("")
-	assert.Error(t, err, "parsing empty string")
+var testsThatReturnErrors = []struct {
+	input       string
+	description string
+}{
+	{"", "parsing empty string"},
+	{"78012345678", "number should be too long"},
+}
+
+func TestStringsThatReturnErrors(t *testing.T) {
+	for _, test := range testsThatReturnErrors {
+		_, err := ParseString(test.input)
+		assert.Error(t, err, test.description)
+	}
 }
 
 func TestSimpleNumber(t *testing.T) {
 	number, err := ParseString("7801234567")
 	assert.NoError(t, err, "Parsing simple number")
 	assert.Equal(t, number, PhoneNumber(7801234567), "Expected parsed number to equal 7801234567")
-}
-
-func TestLongNumberReturnsError(t *testing.T) {
-	_, err := ParseString("78012345678")
-	assert.Error(t, err, "number should be too long")
 }
