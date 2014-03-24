e337c22537c6ee354ab4cefae022bd1286f54f79
Matthias Stone <matthias@clockworkendeavors.com>
2014-03-24 16:20:09 -0600

    Second test
    This test demonstrates the use of t.Errorf, as well as test fall-through. A
    failed tests in go do not stop the test, unless t.Fatal(...) or t.FailNow()
    is called.

--- 2014-03/testing-basics/parser_test.go
+++ 2014-03/testing-basics/parser_test.go
@@ -8,3 +8,13 @@ func TestEmptyStringReturnsError(t *testing.T) {
 		t.Error("Expected parsing empty string to return an error")
 	}
 }
+
+func TestSimpleNumber(t *testing.T) {
+	number, err := ParseString("7801234567")
+	if err != nil {
+		t.Errorf("Unexpected error '%s' when parsing simple number", err)
+	}
+	if number != 7801234567 {
+		t.Errorf("Expected parsed number to equal 7801234567, got %d", number)
+	}
+}
