414a6f594bf11da7e02241d4aab54367c6eeefc5
Matthias Stone <matthias@clockworkendeavors.com>
2014-03-24 16:33:36 -0600

    Make all tests pass

--- 2014-03/testing-basics/phoneparser.go
+++ 2014-03/testing-basics/phoneparser.go
@@ -12,8 +12,11 @@ type PhoneNumber uint64
 func ParseString(s string) (PhoneNumber, error) {
 	number, err := strconv.ParseUint(s, 10, 64)
 
-	if number > 10000000000 {
+	if number >= 10000000000 {
 		return 0, errors.New("number is too long")
+	}
+	if number < 1000000000 {
+		return 0, errors.New("number is too short")
 	}
 
 	return PhoneNumber(number), err
