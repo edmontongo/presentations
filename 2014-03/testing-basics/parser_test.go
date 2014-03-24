817f7cfb8a3027dd561bbd2a5902d8530896272d
Matthias Stone <matthias@clockworkendeavors.com>
2014-03-24 16:01:45 -0600

    Our first failing test
    * Import the testing package to have access to the test suite
    * Name a test function 'Test[Descriptive Name]' and have it accept a pointer to a testing.T struct
    * We can use t.Error to report failures in tests

--- 2014-03/testing-basics/parser_test.go
+++ 2014-03/testing-basics/parser_test.go
@@ -1,1 +1,10 @@
 package phoneparser
+
+import "testing"
+
+func TestEmptyStringReturnsError(t *testing.T) {
+	_, err := ParseString("")
+	if err == nil {
+		t.Error("Expected parsing empty string to return an error")
+	}
+}
