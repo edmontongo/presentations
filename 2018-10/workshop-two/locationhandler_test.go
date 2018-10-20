39a813419f8be37455cdb09b8cc599a9d43eb7d9
Chris Olsen <olsen.chris@gmail.com>
2018-10-20 14:27:41 -0600

    add json test

--- 2018-10/workshop-two/locationhandler_test.go
+++ 2018-10/workshop-two/locationhandler_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -18,7 +19,7 @@ func (f forecasterMock) Forecast(darksky.Location) (*darksky.Weather, error) {
 	return f.weather, f.err
 }
 
-func TestLocationHandlerHTMLResponse(t *testing.T) {
+func TestHTMLResponse(t *testing.T) {
 	tmplCache := newTemplateCache()
 	lh := newLocationHandler(
 		forecasterMock{weather: &darksky.Weather{
@@ -38,5 +39,31 @@ func TestLocationHandlerHTMLResponse(t *testing.T) {
 	body := w.Body.String()
 	if !strings.Contains(body, "hot") {
 		t.Errorf("it should be hot")
+	}
+}
+
+func TestJSONResponse(t *testing.T) {
+	tmplCache := newTemplateCache()
+	lh := newLocationHandler(
+		forecasterMock{weather: &darksky.Weather{
+			Currently: darksky.Currently{Summary: "hot", Temperature: 90},
+		}}, tmplCache,
+	)
+
+	r, _ := http.NewRequest("GET", "/?lat=53.5&lng=-113.3", nil)
+	r.Header.Add("Accept", "application/json")
+	w := httptest.NewRecorder()
+	lh.ServeHTTP(w, r)
+
+	if w.Code != 200 {
+		t.Fatalf("Unexpected status code: %d != 200", w.Code)
+	}
+
+	var weather darksky.Weather
+	if err := json.Unmarshal(w.Body.Bytes(), &weather); err != nil {
+		t.Fatalf("failed unmarshal: %v", err)
+	}
+	if weather.Currently.Temperature != 90 {
+		t.Errorf("it should be 90deg, it was %f", weather.Currently.Temperature)
 	}
 }
